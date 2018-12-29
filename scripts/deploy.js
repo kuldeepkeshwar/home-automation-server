08b3356565becdfaa1d9db2f110c039f123be86a
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2018-12-29 14:49:38 +0530

    fixed deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 const shell = require('shelljs');
 const secrets = require('./secrets.json');
+const { alias } = require('./../now.json');
 
 const envs = `-e FIREBASE_CLIENT_EMAIL=${
   secrets.client_email
 } -e FIREBASE_PRIVATE_KEY="${secrets.private_key}"`;
-shell.exec(`now ${envs}`);
-const url = shell.exec('pbpaste', { silent: true }).stdout;
 
-shell.exec(`now alias ${url} home-automation-server.now.sh`);
+shell.exec(`now ${envs}`);
+
+const url = shell.exec('pbpaste', { silent: true }).stdout;
+shell.exec(`now alias ${url} ${alias[0]}.now.sh`);
