8dc324c433de470169fbe644291df390e063140c
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-02-20 01:17:03 +0530

    updated now script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,6 @@ const envs = `-e FIREBASE_CLIENT_EMAIL=${
   secrets.client_email
 } -e FIREBASE_PRIVATE_KEY="${secrets.private_key}"`;
 
-const url = shell.exec(`now ${envs}`).stdout;
+const url = shell.exec(`now ${envs} --public`).stdout;
 shell.exec(`now alias ${url} ${alias[0]}.now.sh`);
 process.exit(0);
